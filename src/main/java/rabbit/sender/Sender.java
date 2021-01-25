d792eafa22eee6c659bdd6c4cc04853d4a6276df
Hr0ffT <xirrantos@gmail.com>
2021-01-25 16:45:44 +0300

    added props for mq messages

--- src/main/java/rabbit/sender/Sender.java
+++ src/main/java/rabbit/sender/Sender.java
@@ -1,5 +1,7 @@
 package rabbit.sender;
 
+
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import org.apache.log4j.Logger;
 import rabbit.MQConnection;
@@ -10,7 +12,8 @@ public class Sender {
 
     private static final Logger log = Logger.getLogger(Sender.class);
 
-    MQConnection connection;
+    private final MQConnection connection;
+    private final AMQP.BasicProperties basicProperties;
     private final Channel outputChannel;
     private final String ROUTING_KEY;
     private final String EXCHANGE;
@@ -20,6 +23,7 @@ public class Sender {
         this.outputChannel = connection.getOutputChannel();
         this.ROUTING_KEY = connection.getRoutingKey();
         this.EXCHANGE = connection.getExchange();
+        this.basicProperties = buildBasicProperties();
 
     }
 
@@ -31,13 +35,25 @@ public class Sender {
 
         try {
             log.debug(String.format("Publishing to Exchange '%s' with '%s' as Routing Key",EXCHANGE, ROUTING_KEY));
-            outputChannel.basicPublish(EXCHANGE, ROUTING_KEY, null, jsonOutput.getBytes());
+            outputChannel.basicPublish(EXCHANGE, ROUTING_KEY, basicProperties, jsonOutput.getBytes());
             connection.confirm();
         } catch (IOException e) {
             log.error(e);
         }
 
     }
+
+    private static AMQP.BasicProperties buildBasicProperties() {
+
+        return new AMQP.BasicProperties.Builder()
+                .contentType("application/json")
+                .contentEncoding("UTF8")
+                .deliveryMode(Integer.valueOf(System.getenv("MQ_DELIVERY_MODE")))
+                .build();
+
+    }
+
+
 
 
 }
